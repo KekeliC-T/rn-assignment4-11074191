af76e2a6e25edd31d695d0c1ea32857d61861565
KekeliC-T <122158162+KekeliC-T@users.noreply.github.com>
2024-06-19 19:33:03 +0000

    addition of login screen

---
+++ screens/LoginScreen.js
@@ -0,0 +1,51 @@
+// screens/LoginScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+export default function LoginScreen({ navigation }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome Back 👋</Text>
+      <Text>Let's log in. Apply to jobs!</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Button
+        title="Log in"
+        onPress={() => navigation.navigate('Home', { name, email })}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 8,
+  },
+});
